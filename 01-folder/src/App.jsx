9b925c0f2fe27c79eacc11dd334bac47c01bc8dd
pixie <sumayyabelal7@gmail.com>
2026-09-08 23:15:26 +0530

    intro

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+
+const Card = ( {title} ) => {
+const [hasLiked, setHasLiked] = useState(false);
+const [count, setCount] = useState(0);
+
+useEffect( () => {
+  console.log('${title} has been liked: ${hasLiked}');
+}, [hasLiked]);
+
+  return (
+    <div className='card' onClick={() => {
+    setCount(count+1);
+    }}>
+    <h2>{title}<br/>{count}</h2>
+
+    <button onClick={() =>{
+      setHasLiked(!hasLiked)
+    }}> {hasLiked ? '❤️' : '🤍'}
+    </button>
+
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <div>App</div>
+  <div className='card-container'>
+
+  <Card title="Avenger"/>
+  <Card title="Spiderman"/>
+  <Card title="IceAge"/>
+
+    </div>
   )
 }
 
