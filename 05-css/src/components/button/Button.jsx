6df1aaabbf92420ac9fc8a534e520b934b41b275
pixie <sumayyabelal7@gmail.com>
2026-09-12 19:42:25 +0530

    Learn: CSS modules for component styling

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import styles from './button.module.css'
+
+const Button = () => {
+  return (
+    <div>
+      <h3>Pixie</h3>
+   <button className={styles.btn}>Login</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className={styles.header}>Hello</h2>
+      <button>
+        view
+        </button>
+    </div>
+  )
+}
+
+export default Header
